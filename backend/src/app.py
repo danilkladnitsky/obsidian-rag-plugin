5a1fa1dae78fb9f0c9404fc57cc8e20aa75be699
Danil Kladnitsky <danyakladnitsky@gmail.com>
2024-12-16 18:10:11 +0300

    fix: lint backend

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,18 +1,16 @@
+from typing import List
+
 from fastapi import FastAPI, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
+from langchain.schema import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from pydantic import BaseModel
-from typing import List
-from langchain.vectorstores import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.schema import Document
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173",
-    "*"
-]
+origins = ["http://localhost:5173", "*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -48,7 +46,9 @@ class UserNotesStore:
 
     def retrieve_chunks(self, query: str, top_k: int = 5):
         if not self.vectorstore:
-            raise ValueError("Индекс пуст. Добавьте документы перед выполнением запроса.")
+            raise ValueError(
+                "Индекс пуст. Добавьте документы перед выполнением запроса."
+            )
         return self.vectorstore.similarity_search(query, k=top_k)
 
 
@@ -66,23 +66,32 @@ async def add_user_notes(input_data: UserNotesInput):
         user_notes_store.add_documents(input_data.user_id, input_data.documents)
         return {"status": "success"}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Ошибка при добавлении документов: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Ошибка при добавлении документов: {str(e)}"
+        )
 
 
 @app.get("/user-notes/suggestion")
-async def get_user_notes_suggestions(query: str = Query(..., description="Запрос пользователя")):
+async def get_user_notes_suggestions(
+    query: str = Query(..., description="Запрос пользователя"),
+):
     try:
         chunks = user_notes_store.retrieve_chunks(query)
         return {"suggestions": [chunk.page_content for chunk in chunks]}
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Ошибка при извлечении чанков: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Ошибка при извлечении чанков: {str(e)}"
+        )
+
 
 @app.get("/ping")
 async def ping():
     return "pong"
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
