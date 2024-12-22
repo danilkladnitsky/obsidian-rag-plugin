4456e96fbed839455a182f4d7fa7862a4c7376af
maverchenko <mark.averchenko@gmail.com>
2024-12-23 03:44:32 +0500

    backend: upd

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
-OBSIDIDAN_VAULT_PATH = obsidian-vaults/sample-vault
-PLUGIN_PATH = $(OBSIDIDAN_VAULT_PATH)/.obsidian/plugins/plugin
+OBSIDIAN_VAULT_PATH=obsidian-vaults/sample-vault
+PLUGIN_PATH=$(OBSIDIAN_VAULT_PATH)/.obsidian/plugins/plugin
 
 setup-node:
 	cd frontend && npm run setup-node
+
+install:
+	cd fronted && npm install --force
 
 build-plugin:
 	cd frontend && npm run build
@@ -15,4 +18,4 @@ move-plugin:
 	cp -r frontend/dist/* $(PLUGIN_PATH)
 	cp frontend/manifest.json $(PLUGIN_PATH)
 
-update-plugin: setup-node build-plugin reset-plugin move-plugin
+update-plugin: setup-node install build-plugin reset-plugin move-plugin
--- frontend/src/web-app/api/api.ts
+++ frontend/src/web-app/api/api.ts
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { QueryRequest, Suggestion, UserNote } from "web-app/domain"
+import { OllamaEmbeddings } from "@langchain/ollama";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
 
 const wait = (ms: number): Promise<void> => new Promise(r => setTimeout(r, ms))
 
@@ -11,6 +13,47 @@ export interface IAppApi {
 export const MockAppApi: IAppApi = {
     saveNotes: () => wait(2500),
     query: () => wait(1500).then(() => Promise.resolve([{
+        noteUrl: "vault/sample-vault/notes/january/01.md",
+        textContent: "Lorem ipsum"
+    }]))
+}
+
+/**
+ * Splits text into chunks for embedding.
+ * @param {string} text - The text to split.
+ * @param {string} id - The document ID.
+ * @returns {Array} - Array of text chunks.
+ */
+function splitText(text, id) {
+  const maxChunkSize = 500;
+  const overlap = 100;
+  const chunks = [];
+  let startIndex = 0;
+
+  while (startIndex < text.length) {
+    const chunk = text.slice(startIndex, startIndex + maxChunkSize + overlap);
+    chunks.push({ pageContent: chunk, metadata: { id } });
+    startIndex += maxChunkSize;
+  }
+
+  return chunks;
+}
+
+const ollamaEmbeddings = new OllamaEmbeddings({ baseUrl: "http://127.0.0.1:1143", model: "nomic-embed-text:latest" })
+const vectorStore = new MemoryVectorStore(ollamaEmbeddings)
+
+export const FrontApi: IAppApi = {
+	saveNotes: (notes: UserNote[]) => {
+		for (const note of notes) {
+			const splitDocs = splitText(note.content, note.path);
+			await vectorStore.addDocuments(splitDocs);
+			splitDocs.forEach(splitDoc => fileEmbeddings.set(splitDoc.metadata.id, splitDoc));
+		  }
+
+	  console.log('Vector store initialized');
+	},
+
+    query: (query: QueryRequest) => wait(1500).then(() => Promise.resolve([{
         noteUrl: "vault/sample-vault/notes/january/01.md",
         textContent: "Lorem ipsum"
     }]))
